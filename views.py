f25c7fe505637bf1bdd02e4bd79625f121b13854
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2011-11-06 00:16:03 +0530

    zomato request and parsing json response

--- views.py
+++ views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
 from django.shortcuts import render_to_response
+import urllib2
+import json
 
 def index(request):
     return render_to_response('index.html', locals())
@@ -9,5 +11,10 @@ def index(request):
 def diff(request):
     restaurants = []
     for k, v in request.GET.iteritems():
-        restaurants.append(v)
-    return render_to_response('diff.html', locals())
+        url = "https://api.zomato.com/v1/restaurant.json/" + v
+        request = urllib2.Request(url, None, {"X-Zomato-API-Key": "4eb0bc1d9f8015870256524eb0bc1d9f"})
+        response = urllib2.urlopen(request)
+        data = response.read()
+        restaurants.append(json.loads(data))
+        
+    return render_to_response('diff.html', {"data": restaurants})

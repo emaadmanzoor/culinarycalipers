33b32f3802686690ab40a361078a80e4a3787d73
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2011-11-06 11:00:47 +0530

    ranking data generation

--- views.py
+++ views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
 from django.shortcuts import render_to_response
+from django.core.files import File
 import urllib2
 import json
 import re
+import time
 
 def index(request):
     return render_to_response('index.html', locals())
@@ -14,12 +16,11 @@ def diff(request):
     ratings = [-1, -1, -1]
     loaded = False
     for k, v in request.GET.iteritems():
+        index = re.search("\d+", k).group(0)
         if not "rest" in k:
-            index = re.search("\d+", k).group(0)
             ratings[int(index) - 1] = v
             loaded = True
         else:
-            index = re.search("\d+", k).group(0)
             url = "https://api.zomato.com/v1/restaurant.json/" + v
             request = urllib2.Request(url, None, {"X-Zomato-API-Key": "4eb0bc1d9f8015870256524eb0bc1d9f"})
             response = urllib2.urlopen(request)
@@ -29,6 +30,26 @@ def diff(request):
     return render_to_response('diff.html', {"data": zip(restaurants, ratings), "loaded": loaded})
     
 def get_permalink(request):
+    ids = [0, 0, 0]
+    ratings = [-1, -1, -1]
+    
+    for k, v in request.GET.iteritems():
+        index = re.search("\d+", k).group(0)
+        if "rest" in k:
+            ids[int(index) - 1] = v
+        else:
+            ratings[int(index) - 1] = v
+    
+    timestamp = str(int(time.time()))
+    rankdata = {}
+    rankdata[timestamp] = {}
+    for id, rating in zip(ids, ratings):
+        rankdata[timestamp][id] = rating
+    
+    rankfile = File(open('data/ranking.dat', 'a'))
+    rankfile.write(json.dumps(rankdata) + "\n")
+    rankfile.close()
+    
     params = request.GET.urlencode()
     permalink = "http://localhost:8000/diff/?" + params
     return render_to_response('permalink.html', {"permalink" : permalink})
